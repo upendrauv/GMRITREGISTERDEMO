998ae088d5756b825b2ed4efb7f1ab0d9f3e2f8a
upendrauv <upendrapailee123@gmail.com>
2025-07-02 02:06:02 +0530

    Updated files before redeployment

--- src/main/java/com/example/railwaydeployment/controller/UserController.java
+++ src/main/java/com/example/railwaydeployment/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +33,10 @@ public class UserController {
         }
         repo.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body("User registered");
+    }
+    @GetMapping("/xyz")
+    String a()
+    {
+        return "111111111111111111111111111";
     }
 }
