af5c666c50146b249803cfbb6953eedb53fca7a7
upendrauv <upendrapailee123@gmail.com>
2025-07-02 01:41:19 +0530

    Update application.properties and fix deployment settings

--- src/main/java/com/example/railwaydeployment/controller/UserController.java
+++ src/main/java/com/example/railwaydeployment/controller/UserController.java
@@ -5,6 +5,7 @@ package com.example.railwaydeployment.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.railwaydeployment.model.User;
 import com.example.railwaydeployment.repository.UserRepository;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api")
 public class UserController {
